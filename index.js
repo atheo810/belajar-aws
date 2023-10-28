4cd1b4df97c42a4e01290d759a5f794568e803a3
atheo810 <atheos810@gmail.com>
2023-10-28 09:28:40 +0800

    feat: dotenv

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
+const route = require("./routers");
+
 const app = express();
 
+app.use(route);
+
 app.listen(3000, () => {
-  "Running in port localhost:3000";
+  console.log("Running in port localhost:3000");
 });
