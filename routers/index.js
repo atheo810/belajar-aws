ad74bbad4893103cbe8b014c69bdada5bc11f08e
atheo810 <atheos810@gmail.com>
2023-10-28 13:58:29 +0800

    feat: router

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
 
+// import Book Router
+const bookRouter = require("./bookRouter");
+
 const router = Router();
 
 router.get("/", (req, res) => {
   res.send("selamat Datang");
 });
+
+router.use(bookRouter);
 
 module.exports = router;
