c85e4ee77e9cd74dcd8608df4680766d590877cb
atheo810 <atheos810@gmail.com>
2023-10-28 15:14:35 +0800

    feat: bookApiController

---
+++ controllers/bookApiController.js
@@ -0,0 +1,67 @@
+// Prisma
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: "file:../db/data.db",
+    },
+  },
+});
+
+// Class HandleBook
+class BookController {
+  // listing book
+  static async listBook(req, res) {
+    const result = await prisma.book.findMany({});
+
+    res.json(result);
+  }
+
+  // get detail of books
+  static async getDetailBook(req, res) {
+    const result = await prisma.book.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    if (result === null) {
+      res.status(404).json({ message: "Data not found" });
+    } else {
+      res.status(200).json(result);
+    }
+  }
+  //add books
+  static async addbook(req, res) {
+    const result = await prisma.book.create({
+      data: {
+        title: req.body.title,
+        price: req.body.price,
+      },
+    });
+    res.status(201).json(result);
+  }
+
+  // editBook
+  static async editBook(req, res) {
+    const result = await prisma.book.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        title: req.body.title,
+        price: req.body.price,
+      },
+    });
+  }
+
+  // delete Books by Id
+  static async deleteBook(req, res) {
+    const result = await prisma.book.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+  }
+}
+
+module.exports = BookController;
